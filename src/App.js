7df67ad3d1a6c68c7e7cee5cc3a294f0a7717220
p-adams <paadams@nmu.edu>
2018-05-22 22:08:41 -0500

    add api request to github

--- src/App.js
+++ src/App.js
@@ -1,18 +1,32 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import axios from "axios";
+import RepoResults from "./RepoResults";
+import "./App.css";
 
 class App extends Component {
+  state = {
+    results: [],
+    language: "",
+    query: "",
+    labels: []
+  };
+  componentDidMount() {
+    axios
+      .get(
+        `https://api.github.com/search/issues?q=${this.state.query}+label:${
+          this.state.labels[0]
+        }+language:${this.state.language}&sort=created&order=asc`,
+        {
+          headers: { Accept: "application/vnd.github.symmetra-preview+json'" }
+        }
+      )
+      .then(res => console.log(res.data));
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <header>Spiffy</header>
+        <RepoResults />
       </div>
     );
   }
---
+++ src/RepoResults.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class RepoResults extends Component {
+  render() {
+    return <div>We found the following matches</div>;
+  }
+}
+
+export default RepoResults;
