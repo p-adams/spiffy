71c77d64608ec984caafb23fe03e75bea8a22119
p-adams <paadams@nmu.edu>
2018-05-24 22:58:24 -0500

    add result description to RepoResults component

--- src/App.js
+++ src/App.js
@@ -36,18 +36,32 @@ class App extends Component {
         <header>Spiffy</header>
         <div className="resultsContainer">
           {this.state.resultsDidLoad ? (
-            <RepoResults results={this.state.results} />
+            <RepoResults
+              results={this.state.results}
+              query={this.state.query}
+              language={this.state.language}
+            />
           ) : (
             undefined
           )}
         </div>
         <form onSubmit={e => this.makeGithubRequest(e)}>
           <label htmlsfor="topic">Topic (ex: React, Lodash, Kotlin)</label>
-          <input type="text" placeholder="Enter topic..." />
+          <input
+            type="text"
+            value={this.state.query}
+            onChange={e => this.setState({ query: e.target.value })}
+            placeholder="Enter topic..."
+          />
           <label htmlsfor="targetLanguage">
-            Target lanugage (ex: JavaScript, Java, OCaml)
+            Target language (ex: JavaScript, Java, Python, etc)
           </label>
-          <input type="text" placholder="Enter language" />
+          <input
+            type="text"
+            value={this.state.language}
+            onChange={e => this.setState({ language: e.target.value })}
+            placholder="Enter language"
+          />
           <button>Find repos</button>
         </form>
       </div>
--- src/RepoResults.js
+++ src/RepoResults.js
@@ -1,9 +1,19 @@
 import React from "react";
 import ResultList from "./ResultList";
+const spanStyle = {
+  fontWeight: "bold"
+};
 const RepoResults = props => {
   return (
     <div>
-      <h5>We found the following matches</h5>
+      <p>
+        We found{" "}
+        {props.results.length === 1
+          ? props.results.length + " repo"
+          : props.results.length + " repos"}{" "}
+        for <span style={spanStyle}>{props.query}</span>, language{" "}
+        <span style={spanStyle}>{props.language}</span>
+      </p>
       <ResultList results={props.results} />
     </div>
   );
