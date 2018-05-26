0328c629a09f37cf7e74d38605d8a9ad1fb7a13c
p-adams <paadams@nmu.edu>
2018-05-26 01:37:53 -0500

    add tailwind container class to App component

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ class App extends Component {
   state = {
     results: "",
     language: "JavaScript",
-    query: "React",
+    query: "Redux",
     resultsDidLoad: false
   };
   makeGithubRequest = e => {
@@ -32,70 +32,74 @@ class App extends Component {
   };
   render() {
     return (
-      <div className="App">
-        <header>
-          <h1 className="App-Title">Spiffy</h1>
-        </header>
-        <div className="resultsContainer">
-          {this.state.resultsDidLoad ? (
-            <RepoResults
-              results={this.state.results}
-              query={this.state.query}
-              language={this.state.language}
-            />
-          ) : (
-            undefined
-          )}
+      <div className="container h-screen mx-auto border-red-darker border-4">
+        <div className="flex border-red-light border-2">
+          <div className="w-1/5 bg-grey-light h-12" />
+          <div className="w-3/5 bg-grey-dark">
+            <h1>Spiffy</h1>
+            <div className="resultsContainer">
+              {this.state.resultsDidLoad ? (
+                <RepoResults
+                  results={this.state.results}
+                  query={this.state.query}
+                  language={this.state.language}
+                />
+              ) : (
+                undefined
+              )}
+            </div>
+            <form
+              className="w-full max-w-xs"
+              onSubmit={e => this.makeGithubRequest(e)}
+            >
+              <div className="md:flex md:items-center mb-6">
+                <div class="md:w-1/3">
+                  <label
+                    className="block text-yellow font-bold md:text-right mb-1 md:mb-0 pr-4"
+                    htmlfor="topic"
+                  >
+                    Topic
+                  </label>
+                </div>
+                <div className="md:w-2/3">
+                  <input
+                    class="bg-grey-lighter appearance-none border-2 border-grey-lighter hover:border-purple rounded w-full py-2 px-4 text-grey-darker"
+                    type="text"
+                    value={this.state.query}
+                    placeholder="Enter topic..."
+                  />
+                </div>
+              </div>
+              <div className="md:flex md:items-center mb-6">
+                <div class="md:w-1/3">
+                  <label className="block text-yellow font-bold md:text-right mb-1 md:mb-0 pr-4">
+                    Language
+                  </label>
+                </div>
+                <div className="md:w-2/3">
+                  <input
+                    class="bg-grey-lighter appearance-none border-2 border-grey-lighter hover:border-purple rounded w-full py-2 px-4 text-grey-darker"
+                    type="text"
+                    value={this.state.language}
+                    placeholder="Enter language..."
+                  />
+                </div>
+              </div>
+              <div className="md:flex md:items-center">
+                <div className="md:w-1/3" />
+                <div className="md:w-2/3">
+                  <button
+                    className="shadow bg-purple hover:bg-purple-light text-white font-bold py-2 px-4 rounded"
+                    type="submit"
+                  >
+                    Search repos
+                  </button>
+                </div>
+              </div>
+            </form>
+          </div>
+          <div className="w-1/5 bg-gray-darker h-12" />
         </div>
-        <form
-          className="w-full max-w-xs"
-          onSubmit={e => this.makeGithubRequest(e)}
-        >
-          <div className="md:flex md:items-center mb-6">
-            <div class="md:w-1/3">
-              <label
-                className="block text-yellow font-bold md:text-right mb-1 md:mb-0 pr-4"
-                htmlfor="topic"
-              >
-                Topic
-              </label>
-            </div>
-            <div className="md:w-2/3">
-              <input
-                class="bg-grey-lighter appearance-none border-2 border-grey-lighter hover:border-purple rounded w-full py-2 px-4 text-grey-darker"
-                type="text"
-                value={this.state.query}
-                placeholder="Enter topic..."
-              />
-            </div>
-          </div>
-          <div className="md:flex md:items-center mb-6">
-            <div class="md:w-1/3">
-              <label className="block text-yellow font-bold md:text-right mb-1 md:mb-0 pr-4">
-                Language
-              </label>
-            </div>
-            <div className="md:w-2/3">
-              <input
-                class="bg-grey-lighter appearance-none border-2 border-grey-lighter hover:border-purple rounded w-full py-2 px-4 text-grey-darker"
-                type="text"
-                value={this.state.language}
-                placeholder="Enter language..."
-              />
-            </div>
-          </div>
-          <div className="md:flex md:items-center">
-            <div className="md:w-1/3" />
-            <div className="md:w-2/3">
-              <button
-                className="shadow bg-purple hover:bg-purple-light text-white font-bold py-2 px-4 rounded"
-                type="submit"
-              >
-                Search repos
-              </button>
-            </div>
-          </div>
-        </form>
       </div>
     );
   }
